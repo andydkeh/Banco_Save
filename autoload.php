7a6ff40831be3a6f95ea1cfee0e6b706e5eaa5c3
Angel <67561386+andydkeh@users.noreply.github.com>
2021-09-17 16:09:01 -0300

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,11 +1,10 @@
 <?php
 
 spl_autoload_register(function(string $nomeClasse){
-    $classe = str_replace('Projeto\\Banco', 'src', $nomeClasse); //vai pegar o noe da classe e depois vai transformar proj/banco  src;
-    $classe = str_replace('\\', DIRECTORY_SEPARATOR, $classe);
+    $classe  = str_replace('Projeto\\Banco', 'src', $nomeClasse);
+    $classe  = str_replace('\\', DIRECTORY_SEPARATOR, $classe);
     $classe .= '.php';
 
-    if(file_exists($classe)){
+    if (file_exists($classe))
         require_once $classe;
-    }
 });
