54f5e62a25379128275b6bbc88cbefe54266b7ed
Angel <67561386+andydkeh@users.noreply.github.com>
2022-06-19 20:25:43 -0300

    Update Gerente.php

--- src/Modelo/Funcionario/Gerente.php
+++ src/Modelo/Funcionario/Gerente.php
@@ -1,16 +1,16 @@
 <?php
-
-
 namespace Projeto\Banco\Modelo\Funcionario;
 use Projeto\Banco\Modelo\Autenticavel;
 
 class Gerente extends Funcionario implements Autenticavel
 {
-    function calculaBonificacao():float {
+    function calculaBonificacao():float 
+    {
         return $this->getSalario();
     }
 
-    function autenticarOn(string $senha){
+    function autenticarOn(string $senha)
+    {
         return $senha === '22';
     }
 }
